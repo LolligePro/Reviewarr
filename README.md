2d82b791854f28a4d21279b411656097dfdfefe8
Jasper Van de Velde <jasper.vandevelde@ugent.be>
2026-04-13 21:58:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Reviewarr
+Reviewarr is a personal project to self-host a media rating service integrating with [Seerr](https://github.com/seerr-team/seerr) and [Jellyfin](https://github.com/jellyfin/jellyfin).
+
+Work In Progress
---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+api = FastAPI()
+
+
+@api.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@api.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
